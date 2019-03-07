6b22f94b327c4839fafab257b9c7656dda080e1b
Bio Fanirwansyah <43460405+biofanirwansyah@users.noreply.github.com>
2019-03-07 10:28:51 +0700

    Add files via upload

---
+++ addcomponent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class addcomponent : MonoBehaviour
+{
+
+
+
+    public GameObject[] allObjects;
+    private void Start()
+    {
+        allObjects = (GameObject[])Object.FindObjectsOfType(typeof(GameObject));
+        foreach (GameObject obj in allObjects)
+            obj.AddComponent(typeof(objek));
+        foreach (GameObject obj in allObjects)
+            obj.AddComponent(typeof(MeshCollider));
+    }
+       
+            
+}
