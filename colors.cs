f4b80d64c29896d53f31f6701466e13c595c0c5f
Bio Fanirwansyah <43460405+biofanirwansyah@users.noreply.github.com>
2019-03-15 18:59:53 +0700

    Add files via upload

---
+++ colors.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class colors : MonoBehaviour
+{
+    public int red=0;
+    
+    public void merah()
+    {
+        red = 1;
+    }
+}
