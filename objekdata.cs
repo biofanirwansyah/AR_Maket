a54e2fab31df23fd4cc4d9d2cc99b855f9fa2c09
Bio Fanirwansyah <43460405+biofanirwansyah@users.noreply.github.com>
2019-03-22 00:33:32 +0700

    Update objekdata.cs

--- objekdata.cs
+++ objekdata.cs
@@ -11,16 +11,18 @@ public class objekdata
     public float x;
     public float y;
     public float z;
-
-    public objekdata(objek obj)
+    public string names;
+    public objekdata(objek obj,dataobjek dataobj)
     {
-        
         r = obj.r;
         g = obj.g;
         b = obj.b;
         x=obj.transform.localScale.x;
         y=obj.transform.localScale.z;
         z=obj.transform.localScale.z;
-
+        foreach (string str in dataobj.name)
+        {
+            names += str; 
+        }
     }
 }
